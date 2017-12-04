8dc76242f8181a2cd6f1e9aba97704097ac01554
10041004-1 <starsj128@gmail.com>
2017-12-04 17:52:51 +0900

    Music - getter, setter 추가 = sj

--- src/main/java/edu/spring/ex00/domain/Music.java
+++ src/main/java/edu/spring/ex00/domain/Music.java
@@ -13,5 +13,73 @@ public class Music {
 	private String mlyrics;
 
 	public Music() {}
+
+	public int getMid() {
+		return mid;
+	}
+
+	public void setMid(int mid) {
+		this.mid = mid;
+	}
+
+	public String getMtitle() {
+		return mtitle;
+	}
+
+	public void setMtitle(String mtitle) {
+		this.mtitle = mtitle;
+	}
+
+	public String getMartist() {
+		return martist;
+	}
+
+	public void setMartist(String martist) {
+		this.martist = martist;
+	}
+
+	public Date getMdate() {
+		return mdate;
+	}
+
+	public void setMdate(Date mdate) {
+		this.mdate = mdate;
+	}
+
+	public String getMgenre() {
+		return mgenre;
+	}
+
+	public void setMgenre(String mgenre) {
+		this.mgenre = mgenre;
+	}
+
+	public String getMalbum() {
+		return malbum;
+	}
+
+	public void setMalbum(String malbum) {
+		this.malbum = malbum;
+	}
+
+	public int getMcount() {
+		return mcount;
+	}
+
+	public void setMcount(int mcount) {
+		this.mcount = mcount;
+	}
+
+	public String getMlyrics() {
+		return mlyrics;
+	}
+
+	public void setMlyrics(String mlyrics) {
+		this.mlyrics = mlyrics;
+	}
+	
+	
+	//올린다~~~
+	
 	
 }
