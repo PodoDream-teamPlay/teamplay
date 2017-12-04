938b0a6c2362cc2b99ad34acf64af4e4758fe942
dldbwjd7579 <dldbwjd7579@gmail.com>
2017-12-04 17:44:53 +0900

    Music.java 에 변수 추가 - yj

--- src/main/java/edu/spring/ex00/domain/Music.java
+++ src/main/java/edu/spring/ex00/domain/Music.java
@@ -1,6 +1,16 @@
 package edu.spring.ex00.domain;
 
+import java.util.Date;
+
 public class Music {
+	private int mid;
+	private String mtitle;
+	private String martist;
+	private Date mdate;
+	private String mgenre;
+	private String malbum;
+	private int mcount;
+	private String mlyrics;
 
 	public Music() {}
 	
