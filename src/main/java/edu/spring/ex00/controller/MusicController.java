bb2a03d2b6bfe595c06b69894089f76f248bb6e9
seolwoo <seolfeel@naver.com>
2017-12-05 20:48:58 +0900

    리퀘스트 매핑

--- src/main/java/edu/spring/ex00/controller/MusicController.java
+++ src/main/java/edu/spring/ex00/controller/MusicController.java
@@ -1,8 +1,16 @@
 package edu.spring.ex00.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping(value="/podo")
 public class MusicController {
 
+	// 로그 출력 기능
+	private static final Logger logger = 
+			LoggerFactory.getLogger(MusicController.class);
+	
 }
