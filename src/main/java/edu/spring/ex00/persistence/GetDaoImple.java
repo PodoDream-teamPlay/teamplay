a10126035945a9e6bcda8c50f231a47e9650c735
dldbwjd7579 <dldbwjd7579@gmail.com>
2017-12-06 17:56:54 +0900

    PODO-GET 관련 파일 생성 - yj

---
+++ src/main/java/edu/spring/ex00/persistence/GetDaoImple.java
@@ -0,0 +1,8 @@
+package edu.spring.ex00.persistence;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class GetDaoImple implements GetDao {
+
+}
---
+++ src/main/java/edu/spring/ex00/service/GetServiceImple.java
@@ -0,0 +1,8 @@
+package edu.spring.ex00.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GetServiceImple implements GetService {
+
+}
