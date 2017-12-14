6984d20f20dfd4ab32eceaf31a6cb601826ca33f
nohtaegi <ntaeck@naver.com>
2017-12-14 18:21:51 +0900

    point, 도메인 컨트롤러, 다오 ,서비스 등 여러가지 것들을 넣음./ 아직 부족 하지만 차후 추가 예정.

---
+++ src/main/java/edu/spring/ex00/persistence/PointDaoImple.java
@@ -0,0 +1,33 @@
+package edu.spring.ex00.persistence;
+
+import java.util.Date;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import edu.spring.ex00.domain.Point;
+
+@Repository
+public class PointDaoImple implements PointDao {
+
+	@Autowired
+	private SqlSession session;
+	
+	private static final String NAMESPACE = "edu.spring.ex00.Pointmapper";
+	
+	//포인트 적립 시켜야지.
+	@Override
+	public int create(Point point) {
+		System.out.println("pointdaoImple create()");
+		return session.insert(NAMESPACE + ".insert", point);
+	}
+
+	//날짜 비교하기 위해서
+	@Override
+	public Date read(Point point) {
+		System.out.println("pointdaoImple read:()");
+		return session.selectOne(NAMESPACE + ".select", point);
+	}
+
+}
